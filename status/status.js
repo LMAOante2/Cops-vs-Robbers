b4f7aee43e69f449025fd70db9d70e0c473c2ed7
LMAOante2 <matijasicante@gmail.com>
2025-06-06 15:46:04 +0200

    update

--- status/status.js
+++ status/status.js
@@ -137,3 +137,14 @@ function refresh() {
     fetchPlayers();
     updateServerStatus();
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedStatus = localStorage.getItem("status");
+  const content = document.getElementById("igraci");
+
+  if (savedStatus === "true") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+});
