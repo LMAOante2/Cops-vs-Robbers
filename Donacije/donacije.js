8db485a419f9ee37c8f440abb52a862dc78702a2
LMAOante2 <matijasicante@gmail.com>
2025-07-17 12:36:34 +0200

    update

--- Donacije/donacije.js
+++ Donacije/donacije.js
@@ -1,23 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-        const menuToggle = document.querySelector(".menu-toggle");
-        const closeMenu = document.querySelector(".close-menu");
-        const sideMenu = document.querySelector(".side-menu");
-
-        menuToggle.addEventListener("click", function () {
-            sideMenu.classList.add("active");
-        });
-
-        closeMenu.addEventListener("click", function () {
-            sideMenu.classList.remove("active");
-        });
-
-        document.addEventListener("click", function (event) {
-            if (!sideMenu.contains(event.target) && !menuToggle.contains(event.target)) {
-                sideMenu.classList.remove("active");
-            }
-        });
-    });
-
           //Pokazivanje
           const buttons = document.querySelectorAll('.btn4');  
           const rules = document.querySelectorAll('.rule');
--- galerija/galerija.js
+++ galerija/galerija.js
@@ -1,22 +1,3 @@
-        document.addEventListener("DOMContentLoaded", function () {
-        const menuToggle = document.querySelector(".menu-toggle");
-        const closeMenu = document.querySelector(".close-menu");
-        const sideMenu = document.querySelector(".side-menu");
-
-        menuToggle.addEventListener("click", function () {
-            sideMenu.classList.add("active");
-        });
-
-        closeMenu.addEventListener("click", function () {
-            sideMenu.classList.remove("active");
-        });
-
-        document.addEventListener("click", function (event) {
-            if (!sideMenu.contains(event.target) && !menuToggle.contains(event.target)) {
-                sideMenu.classList.remove("active");
-            }
-        });
-    });
     function toggleCars() {
         const carImages = document.getElementById("carImages");
         carImages.classList.toggle("visible");
