7f6f80038edf152bf0745deab5015fcc133d0d7a
LMAOante2 <matijasicante@gmail.com>
2025-10-01 20:46:05 +0200

    Add files via upload

--- profil/profil.js
+++ profil/profil.js
@@ -25,6 +25,9 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore();
 
+const input = document.getElementById("resetEmail");
+const extraText = " - extra";
+
 onAuthStateChanged(auth, (user) => {
   const loggedInUserId = localStorage.getItem('loggedInUserId');
   if (loggedInUserId) {
@@ -37,6 +40,9 @@ onAuthStateChanged(auth, (user) => {
           document.getElementById('loggedUserEmail').innerText = userData.email;
           document.getElementById('loggedUserLName').innerText = userData.lastName;
           document.getElementById('loggedUserposao').innerText = userData.posao;
+          const input = document.getElementById("resetEmail");
+          const extraText = userData.email;
+          input.value = input.value + extraText;
         } else {
           console.log("No document found matching ID");
         }
@@ -107,6 +113,25 @@ onAuthStateChanged(auth, (user) => {
   lightnot.style.display = user ? 'none' : 'true';
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const closeMenu = document.querySelector(".close-menu");
+  const sideMenu = document.querySelector(".side-menu");
+
+  menuToggle.addEventListener("click", function () {
+    sideMenu.classList.add("active");
+  });
+
+  closeMenu.addEventListener("click", function () {
+    sideMenu.classList.remove("active");
+  });
+
+  document.addEventListener("click", function (event) {
+    if (!sideMenu.contains(event.target) && !menuToggle.contains(event.target)) {
+      sideMenu.classList.remove("active");
+    }
+  });
+});
 
 setTimeout(() => {
   document.getElementById('cursor').classList.add('fade-out');
