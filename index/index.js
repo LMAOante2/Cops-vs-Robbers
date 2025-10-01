c2882fea7f1a122e22f848a1f239319d6d3e8821
LMAOante2 <matijasicante@gmail.com>
2025-10-01 20:46:35 +0200

    Add files via upload

--- index/index.js
+++ index/index.js
@@ -19,6 +19,26 @@
         window.location.href = 'index.html';
     }
 
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuToggle = document.querySelector(".menu-toggle");
+        const closeMenu = document.querySelector(".close-menu");
+        const sideMenu = document.querySelector(".side-menu");
+
+        menuToggle.addEventListener("click", function () {
+            sideMenu.classList.add("active");
+        });
+
+        closeMenu.addEventListener("click", function () {
+            sideMenu.classList.remove("active");
+        });
+
+        document.addEventListener("click", function (event) {
+            if (!sideMenu.contains(event.target) && !menuToggle.contains(event.target)) {
+                sideMenu.classList.remove("active");
+            }
+        });
+    });
+
     setTimeout(() => {
         document.getElementById('cursor').classList.add('fade-out');
       }, 3000);
