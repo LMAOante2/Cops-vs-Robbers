7d07d8c23547608aa64bbba4899ace1e10738e03
LMAOante2 <matijasicante@gmail.com>
2025-10-16 22:12:08 +0200

    update

--- login/script.js
+++ login/script.js
@@ -84,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function () {
         icon.classList.remove('fa-eye');
         icon.classList.add('fa-eye-slash');
       } else {
-        input.type = 'password';
+        input.type = 'text';
         icon.classList.remove('fa-eye-slash');
         icon.classList.add('fa-eye');
       }
